2f3689ec9ac0c2396c61fa3b58431b6f5c67d5c0
zhengding <zhengding@suancloud.cn>
2022-12-12 02:32:29 +0800

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# picgo-plugin-gitlab
+
+PicGo Uploader For GitLab
+
+### Install
+
+```bash
+npm i picgo-plugin-gitlab
+```
+
+### Usage
+
+- url: gitlab url (eg:https://gitlab.com)
+- group: gitlab project's group name
+- project: gitlab project name
+- token: gitlab's api token
+- path: img path in response json (eg:url or data.url)
+
+### Demo
+
+![Demo](https://github.com/bugwz/picgo-plugin-gitlab/blob/master/static/demo.jpg?raw=true)
---
+++ src/index.js
@@ -0,0 +1,145 @@
+// fork from : https://github.com/yuki-xin/picgo-plugin-web-uploader
+
+// const logger = require('@varnxy/logger')
+// logger.setDirectory('/Users/zhang/Work/WorkSpaces/WebWorkSpace/picgo-plugin-gitlab/logs')
+// let log = logger('plugin')
+
+const DEFAULT_API_URL = 'https://api.bitbucket.org/2.0'
+const DEFAULT_REPO_URL = 'https://bitbucket.org'
+
+module.exports = (ctx) => {
+  const register = () => {
+    ctx.helper.uploader.register('bitbucket', {
+      handle,
+      name: 'BitBucket图床',
+      config: config
+    })
+  }
+  const handle = async function (ctx) {
+    let userConfig = ctx.getConfig('picBed.bitbucket')
+    if (!userConfig) {
+      throw new Error('Can\'t find uploader config')
+    }
+    // const url = userConfig.URL || DEFAULT_API
+    const group = userConfig.Group
+    const project = userConfig.Project
+    const token = userConfig.Token
+    const srcPath = userConfig.Path
+
+    // const realImgUrlPre = url + '/' + group + '/' + project
+    // const realUrl = url + '/api/v4/projects/' + group + '%2F' + project + '/uploads'
+    const realImgUrlPre = `${DEFAULT_REPO_URL}/${group}/${project}/raw/master`
+    const realUrl = `${DEFAULT_API_URL}/repositories/${group}/${project}/src`
+
+    // const metaUrlPre = `${DEFAULT_API_URL}/repositories/${group}/${project}/src/master`
+
+    try {
+      let imgList = ctx.output
+      for (let i in imgList) {
+        let image = imgList[i].buffer
+        if (!image && imgList[i].base64Image) {
+          image = Buffer.from(imgList[i].base64Image, 'base64')
+        }
+
+        const targetPath = srcPath + imgList[i].fileName
+        const postConfig = postOptions(realUrl, token, targetPath, image, imgList[i].fileName)
+        let body = await ctx.Request.request(postConfig)
+        delete imgList[i].base64Image
+        delete imgList[i].buffer
+        // body = JSON.parse(body)
+        console.log(body)
+        imgList[i]['imgUrl'] = `${realImgUrlPre}${targetPath}`
+
+        // let metaData = await ctx.Request.request({
+        //   method: 'POST',
+        //   url: metaUrlPre
+        // })
+        // metaData = JSON.parse(metaData)
+      }
+    } catch (err) {
+      ctx.emit('notification', {
+        title: '上传失败',
+        body: JSON.stringify(err)
+      })
+    }
+  }
+
+  const postOptions = (url, token, targetPath, image, fileName) => {
+    let headers = {
+      contentType: 'multipart/form-data',
+      'User-Agent': 'PicGo',
+      'Authorization': 'Bearer ' + token
+    }
+    let formData = {
+      [targetPath]: {
+        'value': image,
+        'options': {
+          'filename': fileName
+        }
+      }
+    }
+    const opts = {
+      method: 'POST',
+      url: url,
+      headers: headers,
+      formData: formData
+    }
+    return opts
+  }
+
+  const config = ctx => {
+    let userConfig = ctx.getConfig('picBed.bitbucket')
+    if (!userConfig) {
+      userConfig = {}
+    }
+    return [
+      {
+        name: 'URL',
+        type: 'input',
+        default: userConfig.URL,
+        required: true,
+        message: 'https://bitbucket.org/',
+        alias: 'URL'
+      },
+      {
+        name: 'Group',
+        type: 'input',
+        default: userConfig.Group,
+        required: true,
+        message: 'Group',
+        alias: 'Group'
+      },
+      {
+        name: 'Project',
+        type: 'input',
+        default: userConfig.Project,
+        required: true,
+        message: 'Project',
+        alias: 'Project'
+      },
+      {
+        name: 'Token',
+        type: 'input',
+        default: userConfig.Token,
+        required: true,
+        message: 'project access token',
+        alias: 'Token'
+      },
+      {
+        name: 'Path',
+        type: 'input',
+        default: userConfig.Path,
+        required: false,
+        message: 'src path. such as "/src/images/"',
+        alias: 'Path'
+      }
+    ]
+  }
+  return {
+    uploader: 'bitbucket',
+    // transformer: 'bitbucket',
+    // config: config,
+    register
+
+  }
+}
