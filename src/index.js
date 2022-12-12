f2abcc9554df42e90aad5bfe625426cb66074230
zhengding <zhengding@suancloud.cn>
2022-12-12 22:20:52 +0800

    add remove handle for gui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 // logger.setDirectory('/Users/zhang/Work/WorkSpaces/WebWorkSpace/picgo-plugin-gitlab/logs')
 // let log = logger('plugin')
 
+var FormData = require('form-data')
+
 module.exports = (ctx) => {
   const PLUGIN_ID = 'bitbucket'
   const PLUGIN_NAME = 'BitBucket'
@@ -12,13 +14,71 @@ module.exports = (ctx) => {
   const DEFAULT_API_URL = 'https://api.bitbucket.org/2.0'
   const DEFAULT_REPO_URL = 'https://bitbucket.org'
 
+  // 注册插件
   const register = () => {
+    // 注册上传器
     ctx.helper.uploader.register(PLUGIN_ID, {
       handle,
       name: PLUGIN_NAME,
       config: config
     })
+
+    // 注册图片删除事件处理器
+    ctx.on('remove', (files, guiApi) => {
+      console.log(files, guiApi)
+      handleRemove(ctx, files, guiApi)
+    })
   }
+
+  const handleRemove = async function (ctx, files, guiApi) {
+    let userConfig = ctx.getConfig(PLUGIN_CONFIG_ID)
+    if (!userConfig) {
+      throw new Error('Can\'t find uploader config')
+    }
+    // const url = userConfig.URL || DEFAULT_API
+    const workspace = userConfig.Workspace
+    const project = userConfig.Project
+    const token = userConfig.Token
+    // const srcPath = userConfig.Path
+
+    const realUrl = `${DEFAULT_API_URL}/repositories/${workspace}/${project}/src`
+
+    try {
+      let waitForDeleteFiles = []
+      for (let i in files) {
+        const file = files[i]
+        if (file.type === 'bitbucket') {
+          // https://bitbucket.org/alexzshl/picbed/raw/master/update/test/202212122157493.ico
+          const imgUrl = file.imgUrl
+          // const targetPath = imgUrl.split('/raw/master')[1]
+          const targetPath = imgUrl.split(new RegExp('https://bitbucket.org/.*/raw/master'))[1]
+          waitForDeleteFiles.push(targetPath)
+        }
+      }
+      let formData = new FormData()
+      for (let j in waitForDeleteFiles) {
+        const file = waitForDeleteFiles[j]
+        formData.append('files[]', file)
+      }
+      let body = await ctx.request({
+        url: realUrl,
+        method: 'POST',
+        headers: {
+          contentType: 'multipart/form-data',
+          'User-Agent': 'PicGo',
+          'Authorization': 'Bearer ' + token
+        },
+        formData: formData
+      })
+      console.log(body)
+    } catch (err) {
+      ctx.emit('notification', {
+        title: '删除失败',
+        body: JSON.stringify(err)
+      })
+    }
+  }
+
   const handle = async function (ctx) {
     let userConfig = ctx.getConfig(PLUGIN_CONFIG_ID)
     if (!userConfig) {
@@ -142,6 +202,7 @@ module.exports = (ctx) => {
       }
     ]
   }
+
   return {
     uploader: 'bitbucket',
     // transformer: 'bitbucket',
